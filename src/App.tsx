f89e5e446479c7f349d0e6b4d753797b5359cf71
DaneJacobson <danej@princeton.edu>
2023-10-14 23:52:46 -0400

    Scaffolded the kenken puzzle

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Canvas, useThree } from '@react-three/fiber';
+import * as React from 'react';
+import { OrbitControls } from '@react-three/drei';
 
-function App() {
-  const [count, setCount] = useState(0)
+type BoxProps = {
+  position: [number, number, number];
+}
+
+function Box({ position }: BoxProps) {
+  const boxColor = 0x808080; // Gray
+
+  return (
+    <group position={position}>
+      <mesh>
+        <boxGeometry args={[0.5, 0.5, 0.5]} /> {/* Half in size */}
+        <meshStandardMaterial color={boxColor} transparent opacity={0.3} /> {/* Gray color */}
+      </mesh>
+      <lineSegments>
+        <edgesGeometry attach="geometry">
+          <boxGeometry args={[0.5, 0.5, 0.5]} /> {/* Half in size for the edges as well */}
+        </edgesGeometry>
+        <lineBasicMaterial color={boxColor} />
+      </lineSegments>
+    </group>
+  );
+}
+
+function OrbitGroup({ children }: { children: React.ReactNode }) {
+  const { camera, gl } = useThree();
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <OrbitControls args={[camera, gl.domElement]} />
+      <group>{children}</group>
     </>
   )
 }
 
-export default App
+function App() {
+  const cubes = [];
+  const offset = 0.5; // half the size of a cube (since cube is now 0.5x0.5x0.5)
+
+  for (let x = 0; x < 4; x++) {
+    for (let y = 0; y < 4; y++) {
+      for (let z = 0; z < 4; z++) {
+        cubes.push(
+          <Box
+            key={`${x}-${y}-${z}`}
+            position={[
+              x * offset - (4 - 1) * (offset / 2),
+              y * offset - (4 - 1) * (offset / 2),
+              z * offset - (4 - 1) * (offset / 2),
+            ]}
+          />
+        );
+      }
+    }
+  }
+
+  return (
+    <Canvas>
+      <ambientLight intensity={0.1} />
+      <directionalLight color="red" position={[0, 0, 5]} />
+      <OrbitGroup>
+        {cubes}
+      </OrbitGroup>
+    </Canvas>
+  );
+}
+
+export default App;
